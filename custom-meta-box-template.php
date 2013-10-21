d1a6befc273da22feecbadd0efa962e7b708f391
Julio Potier <juliobosk@gmail.com>
2013-10-21 03:14:07 +0200

    New version, less code, vulnerabilities fixed.
    - I bumped up all filters/actions to create souples with thier
    associated funtions, more simple to read, and avoid back and forth to
    see the function.
    - I created a correct nonce, I would suggest reading Mark Jaquith’s post
    on nonces in WordPress.
    (http://markjaquith.wordpress.com/2006/06/02/wordpress-203-nonces/) A
    nonce is an "object(who)", an "action (what)" and an "unique ID(which
    one)".
    - I did a trick with an array to simply treat the save with corrects key
    instead of copying same blocks of "if isset $_POST..." Remark the usage
    of $$ in $$key and a ternary test.
    - All inputs are named _metas[] and are part of an array, this is part
    of the trick too.
    - All labels and inputs are now correctly linked with for=""
    - All values from input are printed safely using esc_attr (or like) so
    XSS falw is not patched
    - Noo need to check if isset because of the $$key trick, defaults values
    are set.
    - The save method is short now. Many things has change here :
    * we do not need to check wp_is_post_autosave or wp_is_post_revision
    here because if my post is a revision, my data were not saved, why? Thi
    is a mistake so, you just have to test if an AUTOSAVE is not the one
    triggering the hook, that's all. WordPress can update meta with revision
    post's ID. When you enter a post, editing it, for example ou have the ID
    "123", wait few seconds, WP will autosave it, you ID is not "124", so if
    i hit "update", i am a revision. So my data can be saved?? strange!
    Also i check the nonce with check_admin_referer wince you're using
    wp_nonce_field, this is even better, autodie when fail.
    No need to performs 1 check per field. I check if allowed keys, i check
    if the data was postd in $_POST so i update it or i delete it (do not
    update with empty, useless).
    And i use the $_POS['post_ID'] and not the $post_id from hook because
    this one can be a revision id, so as i need it to check the correct
    nonce, i sue the $_POST one.
    
    Questions? @boiteaweb - julio@boiteaweb.fr - julio.boiteaweb (skype)
    ps : i did not check the JS file but the "image upload" button do not
    add the choosen media in the field.

--- custom-meta-box-template.php
+++ custom-meta-box-template.php
@@ -13,33 +13,39 @@ Author URI: http://themefoundation.com/
 /**
  * Adds a meta box to the post editing screen
  */
+add_action( 'add_meta_boxes', 'prfx_custom_meta' );
 function prfx_custom_meta() {
 	add_meta_box( 'prfx_meta', __( 'Meta Box Title', 'prfx-textdomain' ), 'prfx_meta_callback', 'post' );
 }
-add_action( 'add_meta_boxes', 'prfx_custom_meta' );
 
 /**
  * Outputs the content of the meta box
  */
 function prfx_meta_callback( $post ) {
-	wp_nonce_field( basename( __FILE__ ), 'prfx_nonce' );
+	$who = 'prfx_custom_meta';
+	$what = 'update';
+	$which = $post->ID;
+	wp_nonce_field( $who.$what.$which, 'prfx_nonce' );
 	$prfx_stored_meta = get_post_meta( $post->ID );
+	$default_keys = array( 	'meta_text'=>'', 'meta_checkbox'=>'', 'meta_checkbox_two'=>'', 'meta_radio'=>'radio-one', 'meta_select'=>'select-one', 'meta_textarea'=>'', 'meta_color'=>'#FFFFFF', 'meta_image'=>'' );
+	foreach( $default_keys as $key => $def )
+		$$key = isset( $prfx_stored_meta[$key][0] ) ? $prfx_stored_meta[$key][0] : $def;
 	?>
 
 	<p>
 		<label for="meta-text" class="prfx-row-title"><?php _e( 'Example Text Input', 'prfx-textdomain' )?></label>
-		<input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['meta-text'] ) ) echo $prfx_stored_meta['meta-text'][0]; ?>" />
+		<input type="text" id="meta-text" name="_metas[meta_text]" id="meta-text" value="<?php echo esc_attr( $meta_text ); ?>" />
 	</p>
 
 	<p>
 		<span class="prfx-row-title"><?php _e( 'Example Checkbox Input', 'prfx-textdomain' )?></span>
 		<div class="prfx-row-content">
 			<label for="meta-checkbox">
-				<input type="checkbox" name="meta-checkbox" id="meta-checkbox" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox'] ) ) checked( $prfx_stored_meta['meta-checkbox'][0], 'yes' ); ?> />
+				<input type="checkbox" name="_metas[meta_checkbox]" id="meta-checkbox" value="yes" <?php checked( $meta_checkbox, 'yes' ); ?> />
 				<?php _e( 'Checkbox label', 'prfx-textdomain' )?>
 			</label>
 			<label for="meta-checkbox-two">
-				<input type="checkbox" name="meta-checkbox-two" id="meta-checkbox-two" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox-two'] ) ) checked( $prfx_stored_meta['meta-checkbox-two'][0], 'yes' ); ?> />
+				<input type="checkbox" name="_metas[meta_checkbox_two]" id="meta-checkbox-two" value="yes" <?php checked( $meta_checkbox_two, 'yes' ); ?> />
 				<?php _e( 'Another checkbox', 'prfx-textdomain' )?>
 			</label>
 		</div>
@@ -49,11 +55,11 @@ function prfx_meta_callback( $post ) {
 		<span class="prfx-row-title"><?php _e( 'Example Radio Buttons', 'prfx-textdomain' )?></span>
 		<div class="prfx-row-content">
 			<label for="meta-radio-one">
-				<input type="radio" name="meta-radio" id="meta-radio-one" value="radio-one" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-one' ); ?>>
+				<input type="radio" name="_metas[meta_radio]" id="meta-radio-one" value="radio-one" <?php checked( $meta_radio, 'radio-one' ); ?>>
 				<?php _e( 'Radio Option #1', 'prfx-textdomain' )?>
 			</label>
 			<label for="meta-radio-two">
-				<input type="radio" name="meta-radio" id="meta-radio-two" value="radio-two" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-two' ); ?>>
+				<input type="radio" name="_metas[meta_radio]" id="meta-radio-two" value="radio-two" <?php checked( $meta_radio, 'radio-two' ); ?>>
 				<?php _e( 'Radio Option #2', 'prfx-textdomain' )?>
 			</label>
 		</div>
@@ -61,25 +67,25 @@ function prfx_meta_callback( $post ) {
 
 	<p>
 		<label for="meta-select" class="prfx-row-title"><?php _e( 'Example Select Input', 'prfx-textdomain' )?></label>
-		<select name="meta-select" id="meta-select">
-			<option value="select-one" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-one' ); ?>><?php _e( 'One', 'prfx-textdomain' )?></option>';
-			<option value="select-two" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-two' ); ?>><?php _e( 'Two', 'prfx-textdomain' )?></option>';
+		<select name="_metas[meta_select]" id="meta-select">
+			<option value="select-one" <?php selected( $meta_select, 'select-one' ); ?>><?php _e( 'One', 'prfx-textdomain' )?></option>';
+			<option value="select-two" <?php selected( $meta_select, 'select-two' ); ?>><?php _e( 'Two', 'prfx-textdomain' )?></option>';
 		</select>
 	</p>
 
 	<p>
 		<label for="meta-textarea" class="prfx-row-title"><?php _e( 'Example Textarea Input', 'prfx-textdomain' )?></label>
-		<textarea name="meta-textarea" id="meta-textarea"><?php if ( isset ( $prfx_stored_meta['meta-textarea'] ) ) echo $prfx_stored_meta['meta-textarea'][0]; ?></textarea>
+		<textarea name="_metas[meta_textarea]" id="meta-textarea"><?php echo esc_textarea( $meta_textarea ); ?></textarea>
 	</p>
 
 	<p>
 		<label for="meta-color" class="prfx-row-title"><?php _e( 'Color Picker', 'prfx-textdomain' )?></label>
-		<input name="meta-color" type="text" value="<?php if ( isset ( $prfx_stored_meta['meta-color'] ) ) echo $prfx_stored_meta['meta-color'][0]; ?>" class="meta-color" />
+		<input name="_metas[meta_color]" type="text" value="<?php echo esc_attr( $meta_color ); ?>" class="meta-color" />
 	</p>
 
 	<p>
 		<label for="meta-image" class="prfx-row-title"><?php _e( 'Example File Upload', 'prfx-textdomain' )?></label>
-		<input type="text" name="meta-image" id="meta-image" value="<?php if ( isset ( $prfx_stored_meta['meta-image'] ) ) echo $prfx_stored_meta['meta-image'][0]; ?>" />
+		<input type="text" name="_metas[meta_image]" id="meta-image" value="<?php echo esc_attr( $meta_image ); ?>" />
 		<input type="button" id="meta-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'prfx-textdomain' )?>" />
 	</p>
  
@@ -92,81 +98,46 @@ function prfx_meta_callback( $post ) {
 /**
  * Saves the custom meta input
  */
-function prfx_meta_save( $post_id ) {
+add_action( 'save_post', 'prfx_meta_save' );
+function prfx_meta_save() {
  
 	// Checks save status
-	$is_autosave = wp_is_post_autosave( $post_id );
-	$is_revision = wp_is_post_revision( $post_id );
-	$is_valid_nonce = ( isset( $_POST[ 'prfx_nonce' ] ) && wp_verify_nonce( $_POST[ 'prfx_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
- 
-	// Exits script depending on save status
-	if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE || !isset( $_POST['_metas'] ) )
 		return;
-	}
- 
-	// Checks for input and sanitizes/saves if needed
-	if( isset( $_POST[ 'meta-text' ] ) ) {
-		update_post_meta( $post_id, 'meta-text', sanitize_text_field( $_POST[ 'meta-text' ] ) );
-	}
+	$post_id = $_POST['post_ID'];
 
-	// Checks for input and saves
-	if( isset( $_POST[ 'meta-checkbox' ] ) ) {
-		update_post_meta( $post_id, 'meta-checkbox', 'yes' );
-	} else {
-		update_post_meta( $post_id, 'meta-checkbox', '' );
-	}
-	 
-	// Checks for input and saves
-	if( isset( $_POST[ 'meta-checkbox-two' ] ) ) {
-		update_post_meta( $post_id, 'meta-checkbox-two', 'yes' );
-	} else {
-		update_post_meta( $post_id, 'meta-checkbox-two', '' );
-	}
+	$who = 'prfx_custom_meta';
+	$what = 'update';
+	$which = $post_id;
+	check_admin_referer( $who.$what.$which, 'prfx_nonce' );
 
-	// Checks for input and saves if needed
-	if( isset( $_POST[ 'meta-radio' ] ) ) {
-		update_post_meta( $post_id, 'meta-radio', $_POST[ 'meta-radio' ] );
-	}
+	$allowed_keys = array_flip( array( 'meta_text', 'meta_checkbox', 'meta_checkbox_two', 'meta_radio', 'meta_select', 'meta_textarea', 'meta_color', 'meta_image' ) );
 
-	// Checks for input and saves if needed
-	if( isset( $_POST[ 'meta-select' ] ) ) {
-		update_post_meta( $post_id, 'meta-select', $_POST[ 'meta-select' ] );
-	}
-
-	// Checks for input and saves if needed
-	if( isset( $_POST[ 'meta-textarea' ] ) ) {
-		update_post_meta( $post_id, 'meta-textarea', $_POST[ 'meta-textarea' ] );
-	}
-
-	// Checks for input and saves if needed
-	if( isset( $_POST[ 'meta-color' ] ) ) {
-		update_post_meta( $post_id, 'meta-color', $_POST[ 'meta-color' ] );
-	}
-
-	// Checks for input and saves if needed
-	if( isset( $_POST[ 'meta-image' ] ) ) {
-		update_post_meta( $post_id, 'meta-image', $_POST[ 'meta-image' ] );
-	}
-
+	$final_keys = array_intersect_key( $_POST['_metas'], array_flip( $allowed_keys ) );
+	foreach( $allowed_keys as $key=>$value )
+		if( isset( $_POST['_metas'][$key] ) )
+			update_post_meta( $post_id, $key, $_POST['_metas'][$key] );
+		else
+			delete_post_meta( $post_id, $key );
 }
-add_action( 'save_post', 'prfx_meta_save' );
 
 
 /**
  * Adds the meta box stylesheet when appropriate
  */
+add_action( 'admin_print_styles', 'prfx_admin_styles' );
 function prfx_admin_styles(){
 	global $typenow;
 	if( $typenow == 'post' ) {
 		wp_enqueue_style( 'prfx_meta_box_styles', plugin_dir_url( __FILE__ ) . 'meta-box-styles.css' );
 	}
 }
-add_action( 'admin_print_styles', 'prfx_admin_styles' );
 
 
 /**
  * Loads the color picker javascript
  */
+add_action( 'admin_enqueue_scripts', 'prfx_color_enqueue' );
 function prfx_color_enqueue() {
 	global $typenow;
 	if( $typenow == 'post' ) {
@@ -174,11 +145,11 @@ function prfx_color_enqueue() {
 		wp_enqueue_script( 'meta-box-color-js', plugin_dir_url( __FILE__ ) . 'meta-box-color.js', array( 'wp-color-picker' ) );
 	}
 }
-add_action( 'admin_enqueue_scripts', 'prfx_color_enqueue' );
 
 /**
  * Loads the image management javascript
  */
+add_action( 'admin_enqueue_scripts', 'prfx_image_enqueue' );
 function prfx_image_enqueue() {
 	global $typenow;
 	if( $typenow == 'post' ) {
@@ -195,4 +166,3 @@ function prfx_image_enqueue() {
 		wp_enqueue_script( 'meta-box-image' );
 	}
 }
-add_action( 'admin_enqueue_scripts', 'prfx_image_enqueue' );
